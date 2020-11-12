37f65f31161736f556c8fd8a275acbecbfce8923
Ody Mbegbu <odytrice@gmail.com>
2020-11-12 10:33:12 +0000

    Reset Migrations and Added Rooms

--- meeting_planner/meetings/admin.py
+++ meeting_planner/meetings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from meetings.models import Meeting
+from meetings.models import Meeting, Room
 
 # Register your models here.
 admin.site.register(Meeting)
+admin.site.register(Room)
--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,7 +1,25 @@
 from django.db import models
+from datetime import time
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}: room {self.number} on floor {self.floor}"
+
 
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
